2224b35dfd93d689b818b4a89ec1e2bff420528c
Darya Pylypenko <daryap101@gmail.com>
2025-04-24 13:24:41 -0400

    slay

---
+++ next.config.js
@@ -0,0 +1,9 @@
+```javascript
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['i.ebayimg.com'],
+  },
+};
+module.exports = nextConfig;
+```
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
+```javascript
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: { extend: {} },
+   content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}'
+  ],
+  theme: {
+    extend: {
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' }
+        }
+      }
+    }
+  },
   plugins: []
 };
