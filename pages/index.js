bfecc467db47dc24f3599b44acda49a76b2b1bfb
Darya Pylypenko <daryap101@gmail.com>
2025-04-23 22:03:41 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="max-w-4xl mx-auto p-6">
+      <h2 className="text-3xl font-bold mb-4">About Coppola Artistica</h2>
+      <p>Founded in Sorrento, Italy over 60 years ago by my grandparents.</p>
+      <p>We came to America 20 years ago, bringing handcrafted Italian pottery to Venice, FL.</p>
+      <p className="mt-4">Address: 213 W Venice Ave Unit B, Venice FL 34285</p>
+      <div className="mt-6 flex space-x-4">
+        <a href="https://www.instagram.com/coppola_artistica" target="_blank">Instagram</a>
+        <a href="https://www.ebay.com/str/coppolaartimports2014" target="_blank">eBay</a>
+        <a href="https://www.etsy.com/shop/CoppolaArtistica" target="_blank">Etsy</a>
+      </div>
+    </main>
+  );
+}
